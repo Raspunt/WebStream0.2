73e16296f15af972a4fc2bd99cb36fd943a2047e
Raspunt <m_ryabinin@inbox.ru>
2022-10-29 19:23:47 +0100

    buttons

--- static/js/keyboardListner.js
+++ static/js/keyboardListner.js
@@ -3,24 +3,7 @@
 
 
 			
-function SendMotorSignal (comm){
 
-	var fd = new FormData();
-	fd.append('mc', comm);
-
-    axios({
-    method: "post",
-    url: "/motor_command",
-    data:fd,
-    headers: { "Content-Type": "multipart/form-data" },
-    })
-  .then(function (response) {
-    //handle success
-  })
-  .catch(function (response) {
-    //handle error
-  });	
-}
 
 document.addEventListener('keydown', (event) => {
   var name = event.key;
